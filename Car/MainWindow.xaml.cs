5299f9429adbafc2d0730030de4ed9b8ab10fc88
Jan Čejka <Cejka.Jan@stud.vassboskovice.cz>
2022-05-03 09:00:16 +0200

    Přidejte soubory projektu.

---
+++ Car/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Car
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        camion Scania;
+        camion Mercedes;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Scania = new camion(10000, 200, 30);
+            Scania.Znacka = "Scania";
+
+            Mercedes = new camion(10000, 220, 32);
+            Mercedes.Znacka = "Mercedes";
+
+            kamion1.Text = Scania.Znacka + "\n";
+            kamion1.Text = "Nosnost" + Scania.Nosnost + "\n";
+            kamion1.Text = "Naložení" + Scania.Naložení + "\n";
+            kamion1.Text = "Spotreba" + Scania.Spotreba + "\n";
+            kamion1.Text = "ObjemNadrze" + Scania.ObjemNádrže + "\n";
+            kamion1.Text = "Palivo" + Scania.Palivo + "\n";
+            kamion1.Text = "Vzdálenost" + Scania.Vzdalenost + "\n";
+
+        }
+        
+
+    }
+}
---
+++ Car/camion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    internal class camion
+    {
+
+        public string Znacka { get; set; }
+        public int Nosnost { get; set; }
+        public int Naložení { get; set; }
+        public int ObjemNádrže { get; set; }
+        public int Palivo { get; set; }
+        public int Spotreba { get; set; }
+        public int Vzdalenost { get; set; }
+
+        public camion(int nosnost, int objemnadrze, int spotreba)
+        {
+            Znacka = "";
+            Nosnost = nosnost;
+            Naložení = 0;
+            ObjemNádrže = objemnadrze;
+            Palivo = 0;
+            Spotreba = spotreba;
+            Vzdalenost = 0;
+        }
+
+
+        public void Jet(int Vzdalenost)
+        {
+            if (Vzdalenost )
+            {
+
+            }   
+        }
+
+        public void Naloz()
+        {
+
+        }
+        public void Vyloz()
+        {
+
+        }
+
+        public void Tankovat()
+        {
+            Palivo += input1.Text ;
+            if (Palivo > ObjemNádrže)
+            {
+                Palivo = ObjemNádrže;
+            }
+        }
+    }
+}
